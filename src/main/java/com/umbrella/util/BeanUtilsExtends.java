eb2cdaf50baeb162295be405e2cf41b1080d125b
reagan <reagan@reagan-PC>
2016-07-29 18:04:27 +0800

    members

---
+++ src/main/java/com/umbrella/util/BeanUtilsExtends.java
@@ -0,0 +1,75 @@
+package com.umbrella.util;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.Converter;
+
+import com.umbrella.worker.result.ResultDO;
+import com.umbrella.worker.result.ResultSupport;
+
+
+
+public class BeanUtilsExtends {
+	static {
+		//ConvertUtils.register(new DateConverter(), java.util.Date.class);
+		ConvertUtils.register(new Converter() {
+			public Object convert(Class type, Object value) {
+				return value == null ? null : value;
+			}
+		}, java.util.Date.class);
+		
+		ConvertUtils.register(new Converter() {
+			public Object convert(Class type, Object value) {
+				return value == null ? null : value;
+			}
+		}, java.lang.Integer.class);
+
+		ConvertUtils.register(new Converter() {
+			public Object convert(Class type, Object value) {
+				return value == null ? null : value;
+			}
+		}, java.lang.Long.class);
+	}
+
+	/*public static void copyProperties(Object dest, Object orig) {
+		try {
+			BeanUtils.copyProperties(dest, orig);
+		} catch (IllegalAccessException ex) {
+			ex.printStackTrace();
+		} catch (InvocationTargetException ex) {
+			ex.printStackTrace();
+		}
+	}*/
+	
+	/**
+	 * 拷贝数据到DTO对象
+	 * 
+	 * @param dest 目标DTO
+	 * @param orig 源DAO
+	 * @return 是否拷贝成功 true成功false失败
+	 */
+	public static boolean copyProperties(Object dest, Object orig) {
+		try {
+			BeanUtils.copyProperties(dest, orig);
+		} catch (Exception ex) {
+			return false;
+		}
+		return true;
+	}
+	
+	/* 本来就是个简单的调用还包装一个方法来这就是脱了裤子放屁
+	 * 拷贝DTO到实体对象
+	 */
+	public static ResultSupport copy(Object dest, Object orig) {
+		ResultSupport result = new ResultSupport();
+		try {
+			BeanUtils.copyProperties(dest, orig);
+		} catch (Exception e) {
+			result.setSuccess(false);
+		    result.setErrorCode(ResultDO.PROPERTY_VALUE_SET_ERROR);
+		    result.setErrorMsg(ResultDO.PROPERTY_VALUE_SET_ERROR_MSG);
+		}
+		return result;
+	}
+
+}
--- src/main/java/com/umbrella/worker/service/impl/MemberServiceImpl.java
+++ src/main/java/com/umbrella/worker/service/impl/MemberServiceImpl.java
@@ -1,25 +1,111 @@
 package com.umbrella.worker.service.impl;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.umbrella.util.BeanUtilsExtends;
 import com.umbrella.worker.dao.WMembersMapper;
 import com.umbrella.worker.dto.MembersDO;
+import com.umbrella.worker.entity.WMembers;
+import com.umbrella.worker.entity.WMembersExample;
 import com.umbrella.worker.query.MembersQuery;
 import com.umbrella.worker.result.ResultDO;
+import com.umbrella.worker.result.ResultSupport;
 import com.umbrella.worker.service.IMemberService;
 
 public class MemberServiceImpl implements IMemberService {
+	
+	private static Logger logger = Logger.getLogger(MemberServiceImpl.class);
 	
 	private WMembersMapper membersMapper;
 
 	@Override
 	public ResultDO register(MembersDO membersDO) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		WMembers members = new WMembers();
+		
+		ResultSupport result = BeanUtilsExtends.copy(membersDO, members);
+		
+		if(!result.isSuccess()) {
+			return result;
+		}
+		
+		int recordNum = -1;
+		
+		members.setwMRegisterTime(Calendar.getInstance().getTime());
+		members.setDatalevel(1);
+		members.setStatus(1);
+		members.setCreateTime(Calendar.getInstance().getTime());
+		members.setModifiTime(Calendar.getInstance().getTime());
+		
+		try {
+			recordNum = membersMapper.insertSelective(members);
+		} catch (Exception e) {
+			result.setSuccess(false);
+			result.setErrorMsg(ResultDO.SYSTEM_EXCEPTION_ERROR_MSG);
+			result.setErrorCode(ResultDO.SYSTEM_EXCEPTION_ERROR);
+			logger.error("[obj:catalogs][opt:create][msg:" + e.getMessage()
+			+ "]");
+			return result;
+		}
+		
+		if(recordNum == 1) {
+			result.setModel(ResultDO.FIRST_MODEL_KEY, members.getId());
+		} else {
+			result.setSuccess(false);
+		}
+		return result;
 	}
 
 	@Override
 	public ResultDO login(MembersQuery membersQuery) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ResultSupport result = new ResultSupport();
+		
+		WMembersExample example = new WMembersExample();
+		
+		example.createCriteria()
+			.andWMMobileEqualTo(membersQuery.getMobile())
+			.andWMPasswordEqualTo(membersQuery.getPassword())
+			.andDatalevelNotEqualTo(-1);
+		List<WMembers> list = null;
+		try {
+			list = membersMapper.selectByExample(example);
+		} catch (Exception e) {
+            result.setSuccess(false);
+            result.setErrorCode(ResultDO.SYSTEM_EXCEPTION_ERROR);
+            result.setErrorMsg(ResultDO.SYSTEM_EXCEPTION_ERROR_MSG);
+            logger.error("[obj:member][opt:login][msg:"+e.getMessage()+"]");
+            return result;
+		}
+		
+		if(list != null && list.isEmpty()) {
+			if(list.size() == 1) {
+				MembersDO membersDO = getMemberDO(list.get(0));
+				if(membersDO != null) {
+					result.setModel(ResultSupport.FIRST_MODEL_KEY, membersDO);
+				} else {
+					result.setSuccess(false);
+					result.setErrorCode(ResultDO.MEMBER_LOGIN_FAILED);
+		            result.setErrorMsg(ResultDO.MEMBER_LOGIN_FAILED_MSG);
+					return result;
+				}
+			} else {
+				result.setSuccess(false);
+				result.setErrorCode(ResultDO.MEMBER_LOGIN_FAILED);
+	            result.setErrorMsg(ResultDO.MEMBER_LOGIN_FAILED_MSG);
+				return result;
+			}
+		} else {
+			result.setSuccess(false);
+			result.setErrorCode(ResultDO.MEMBER_LOGIN_FAILED);
+            result.setErrorMsg(ResultDO.MEMBER_LOGIN_FAILED_MSG);
+			return result;
+		}
+		return result;
 	}
 
 	@Override
@@ -44,6 +130,29 @@ public class MemberServiceImpl implements IMemberService {
 	public ResultDO list(MembersDO membersDO) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private MembersDO getMemberDO(WMembers obj) {
+		if(obj == null) return null;
+		MembersDO dst = new MembersDO();
+		return BeanUtilsExtends.copyProperties(dst, obj) ? dst : null;
+	}
+	
+	private List<MembersDO> getMemberDOList(List<WMembers> list) {
+		List<MembersDO> resultList = new ArrayList<MembersDO>();
+		if(list != null && list.isEmpty()) {
+			for(WMembers members : list) {
+				MembersDO memberDO = this.getMemberDO(members);
+				if(memberDO != null) {
+					resultList.add(memberDO);
+				} else {
+					return null;
+				}
+			}
+		} else {
+			return null;
+		}
+		return resultList;
 	}
 
 }
