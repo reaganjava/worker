39526c572a930b3a10ab1a4d6f725b31d7755101
reagan <reagan@reagan-PC>
2016-08-03 15:51:18 +0800

    m

---
+++ src/main/java/com/umbrella/worker/service/impl/ITransactionService.java
@@ -0,0 +1,18 @@
+package com.umbrella.worker.service.impl;
+
+import com.umbrella.worker.dto.TransactionDO;
+import com.umbrella.worker.query.TransactionQuery;
+import com.umbrella.worker.result.ResultDO;
+
+public interface ITransactionService {
+
+	public ResultDO create(TransactionDO transactionDO);
+	
+	public ResultDO modifi(TransactionDO transactionDO);
+	
+	public ResultDO remove(int transactionId);
+	
+	public ResultDO get(int transactionId);
+	
+	public ResultDO list(TransactionQuery transactionQuery);
+}
