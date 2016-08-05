c72afc1a9c04bc913a1c464769e840a3a2c45af2
reagan <reagan@reagan-PC>
2016-08-05 18:40:27 +0800

    m

--- src/main/java/com/umbrella/worker/web/controller/GoodsController.java
+++ src/main/java/com/umbrella/worker/web/controller/GoodsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.umbrella.worker.dto.OrderDetailDO;
 import com.umbrella.worker.dto.WorkerTaskDO;
 import com.umbrella.worker.query.WorkerTaskQuery;
 import com.umbrella.worker.result.ResultDO;
