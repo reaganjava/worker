51ada2d938507c20bc66aba62973e55800a5908b
reaganjava@gmail.com <reaganjava@gmail.com>
2016-08-05 10:11:48 +0800

    m

--- src/main/java/com/umbrella/worker/web/controller/MembersController.java
+++ src/main/java/com/umbrella/worker/web/controller/MembersController.java
@@ -4,10 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.umbrella.worker.dto.MemberDetailDO;
 import com.umbrella.worker.dto.MembersDO;
 import com.umbrella.worker.result.ResultDO;
 import com.umbrella.worker.result.ResultSupport;
@@ -55,7 +57,7 @@ public class MembersController {
 		ResultDO resultDO = memberService.create(membersDO);
 		
 		if(resultDO.isSuccess()) {
-			request.getSession().setAttribute("MEMBER_LOGIN_INFO", resultDO.getModel(ResultSupport.FIRST_MODEL_KEY));
+			request.getSession().setAttribute("MEMBER_ID", resultDO.getModel(ResultSupport.FIRST_MODEL_KEY));
 			mav.setViewName(backPage);
 		} else {
 			mav.setViewName("errer");
@@ -89,11 +91,76 @@ public class MembersController {
 		ResultDO resultDO = memberService.validate(membersDO);
 		
 		if(resultDO.isSuccess()) {
-			request.getSession().setAttribute("MEMBER_LOGIN_INFO", resultDO.getModel(ResultSupport.FIRST_MODEL_KEY));
+			membersDO = (MembersDO) resultDO.getModel(ResultSupport.FIRST_MODEL_KEY);
+			request.getSession().setAttribute("MEMBER_ID", membersDO.getId());
 			mav.setViewName(backPage);
 		} else {
 			mav.setViewName("error");
 		}
 		return mav;
+	}
+	
+	@RequestMapping(value = "/accountPassword.html", method = RequestMethod.GET)
+	public ModelAndView editPwd(ModelAndView mav,  
+			HttpServletRequest request) {
+		mav.setViewName("members/accountPassword");
+		return mav; 
+	}
+	
+	@RequestMapping(value = "/accountPassword.html", method = RequestMethod.POST)
+	public ModelAndView editPwd(ModelAndView mav,  
+			MembersDO membersDO, HttpServletRequest request) {
+		
+		if(!membersDO.getRePassword()
+				.equals(membersDO.getwMPassword())) {
+			mav.setViewName("members/accountPassword");
+		}
+		
+		MD5 md5 = new MD5();
+		String md5Pwd = md5.getMD5ofStr(membersDO.getwMPassword() 
+				+ membersDO.getwMMobile());
+		membersDO.setwMPassword(md5Pwd);
+		ResultDO resultDO = memberService.modifiPwd(membersDO);
+		
+		if(resultDO.isSuccess()) {
+			mav.setViewName("members/login");
+		} else {
+			mav.setViewName("error");
+		}
+		
+		return mav; 
+	}
+	
+	@RequestMapping(value = "/accountInfo.html", method = RequestMethod.GET)
+	public ModelAndView accountInfo(ModelAndView mav, 
+			HttpServletRequest request) {
+		
+		Integer id = (Integer) request.getSession().getAttribute("MEMBER_ID");
+		ResultDO resultDO = memberService.get(id);
+		if(resultDO.isSuccess()) {
+			mav.addObject("MEMBER_INFO", resultDO.getModel(ResultSupport.FIRST_MODEL_KEY));
+			mav.setViewName("members/accountInfo");
+		} else {
+			mav.setViewName("error");
+		}
+		
+		return mav; 
+	}
+	
+	@RequestMapping(value = "/change.html", method = RequestMethod.POST)
+	public ModelAndView change(ModelAndView mav, 
+			MemberDetailDO memberDetailDO,
+			HttpServletRequest request) {
+		
+		
+		ResultDO resultDO = memberService.modifi(memberDetailDO);
+		
+		if(resultDO.isSuccess()) {
+			mav.setViewName("members/accountInfo");
+		} else {
+			mav.setViewName("error");
+		}
+		
+		return mav; 
 	}
 }
